4f241b761eea86eb8cfe231d32db043c37e33b80
Hiago <hiagoleitao@gmail.com>
2020-04-05 23:23:20 -0300

    Atividade concluida

---
+++ index.js
@@ -0,0 +1,16 @@
+const soap = require('soap')
+
+const url = 'https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl'
+
+soap.createClient(url, (err, client) => {
+    if(err){
+        console.log(err)
+    } else {
+        console.log('')
+        client.consultaCEP({
+           cep: '58704230' 
+        }, (err, res) => {
+            console.log(res)
+        })
+    }    
+})
